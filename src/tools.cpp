37f9ceda7c3199720dc2cb54162f282cce1fa6d0
Emil Wåreus <emil.wareus47@gmail.com>
2018-02-17 13:50:16 +0800

    yo

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,10 +15,11 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	VectorXd rmse(4);
 	rmse << 0, 0, 0, 0;
 
-	if (estimations.size() != ground_truth.size()
+	if (estimations.size() != ground_truth.size() {
 		|| estimations.size() == 0) {
 		cout << "Size not matching RMSE" << endl;
 		return rmse;
+	}
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {

86f1cc02d637dd06f9d072a49ff48c5775513888
Emil Wåreus <emil.wareus47@gmail.com>
2018-02-18 12:06:02 +0800

    RMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -18,6 +18,30 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<
 		cout << "Size not matching RMSE" << endl;
 		return rmse;
 	}
+
+
+
+
+	//RMSE <= [.11, .11, 0.52, 0.52]
+
+	//accumulate squared residuals
+	for (unsigned int i = 0; i < estimations.size(); ++i) {
+
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse / estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+	
+	return rmse;
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
